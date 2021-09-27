8dc463c47b76749c79f872f4d53e4550bf1eb94c
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2021-09-27 09:37:36 +0200

    fix

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -16,3 +16,4 @@ console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
 console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+module.exports = splitArrayInGroups;
