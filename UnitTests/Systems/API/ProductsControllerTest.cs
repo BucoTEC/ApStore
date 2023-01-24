36f5609df7ee8978c2ad32c1dc84bd9081691326
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2023-01-24 23:06:24 +0100

    add new tes

---
+++ UnitTests/Systems/API/ProductsControllerTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Systems.API
+{
+    public class ProductsControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
